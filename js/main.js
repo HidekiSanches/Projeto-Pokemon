24d05640e82a306b1c0b0fec054a1658d820a871
HidekiSanches <thiago.sanches@sptech.school>
2022-12-18 21:23:17 -0300

    pagina inicial finalizada

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-function iniciar(){
-    inicio.style="display: none;"
-    loding.style="display: flex;"
+function iniciar() {
+  inicio.style = "display: none;";
+  loding.style = "display: flex;";
 
-    setTimeout(() => {
-        loding.style = `display: none;`;
-        header.style = `display: flex;`;
-      }, "2000");
+  setTimeout(() => {
+    window.location.href = "inicial.html";
+  }, "2000");
+}
+
+function readMore1() {
+  read1.style = "display: none;";
+  hidden1.style = "overflow: auto;";
+}
+
+function readMore2() {
+  read2.style = "display: none;";
+  hidden2.style = "overflow: auto;";
+}
+
+function readMore3() {
+  read3.style = "display: none;";
+  hidden3.style = "overflow: auto;";
 }
